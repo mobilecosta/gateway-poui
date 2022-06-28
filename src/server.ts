1c4753def0ba1dfbceb53998e47376bbddb938c3
Tiago-msC <tiagomscunha@outlook.com>
2022-06-28 14:09:53 -0300

    feat: mudando a porta de execução do gateway.

--- src/server.ts
+++ src/server.ts
@@ -2,7 +2,7 @@ import express from 'express';
 import cors from 'cors';
 import router from './routes/gateway-routes';
 
-const PORT = process.env.PORT_HTTPS_PORTAL || 8080;
+const PORT = process.env.PORT_HTTPS_PORTAL || 4000;
 const HOSTNAME = process.env.HOSTNAME || 'http://localhost';
 const app = express();
 
